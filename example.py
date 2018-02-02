2d49b22e8adcff731733ed58626cd9392cdb8af7
aatherton <aatherton1990@live.com>
2018-02-01 20:57:35 -0600

    pushing example as requested

---
+++ example.py
@@ -0,0 +1,6 @@
+result = {}
+api_endpoints = [list thing here]
+for each in player:
+	result[f"{player_id}_{team_id}"] = {}
+	for other in api_endpoints:
+		result[f"{player_id}_{team_id}"][other] = {player_input[other]}
