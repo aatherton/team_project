8745aa82946cb7e5c6087a0569d81023e62d5ab8
Blaine Anspaugh <spaugh56@yahoo.com>
2018-02-09 20:19:57 -0600

    BMA graphs
    BMA graphs

--- example.py
+++
@@ -1,6 +0,0 @@
-result = {}
-api_endpoints = [list thing here]
-for each in player:
-	result[f"{player_id}_{team_id}"] = {}
-	for other in api_endpoints:
-		result[f"{player_id}_{team_id}"][other] = {player_input[other]}
